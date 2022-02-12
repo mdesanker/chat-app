ce75d30e429f3b6c58daf853b38c74bec14490f9
Michael Desanker <mdesanker@gmail.com>
2022-02-11 21:55:11 -0500

    Set up socket connection

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 
 // App set up
 const app = express();
@@ -8,3 +10,34 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 const server = app.listen(5000, () => console.log("Listening on port 5000"));
+
+// Socket setup
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(server);
+
+io.on("connection", (socket) => {
+  console.log("Made socket connection", socket.id);
+});
---
+++ public/script.js
@@ -0,0 +1,3 @@
+// Make connection
+const socket = io();
+// const socket = io.connect("http://localhost:5000");

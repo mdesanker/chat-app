3774b59797475458c82126c2a63aa48a46e9c401
Michael Desanker <mdesanker@gmail.com>
2022-02-11 21:35:41 -0500

    Serve index.html static file

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import path from "path";
 
 // App set up
 const app = express();
 
-app.listen(5000, () => console.log("Listening on port 5000"));
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = app.listen(5000, () => console.log("Listening on port 5000"));

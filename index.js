1402b49c01c4b91bbbcaf5d02cae75bc0bd5b88e
Michael Desanker <mdesanker@gmail.com>
2022-02-12 05:10:58 -0500

    Emit messages to clients

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const socket = require("socket.io");
+
+// App set up
+const app = express();
+
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = app.listen(5000, () => console.log("Listening on port 5000"));
+
+// Socket setup
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("Made socket connection", socket.id);
+
+  // Listening
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data);
+  });
+});
--- public/script.js
+++ public/script.js
@@ -1,3 +1,22 @@
 // Make connection
 const socket = io();
 // const socket = io.connect("http://localhost:5000");
+
+// Elements
+const message = document.querySelector("#message");
+const handle = document.querySelector("#handle");
+const btn = document.querySelector("#send");
+const output = document.querySelector("#output");
+
+// Emit events
+btn.addEventListener("click", () => {
+  socket.emit("chat", {
+    handle: handle.value,
+    message: message.value,
+  });
+});
+
+// Listen for events
+socket.on("chat", (data) => {
+  output.innerHTML += `<p><strong>${data.handle}</strong> ${data.message}</p>`;
+});
